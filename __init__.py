02d25c5ac535d858e13c11caacbfcd6f18b65899
maxbah <51990390+maxbah@users.noreply.github.com>
2022-02-08 09:53:18 +0200

    Add files via upload
    Add get_metadata function + tests

---
+++ __init__.py
@@ -0,0 +1,7 @@
+from .file_service import create_file
+from .file_service import read_file
+from .file_service import delete_file
+from .file_service import change_dir
+from .file_service import list_dir
+from .file_service import get_cwd
+from .file_service import get_file_metadata
